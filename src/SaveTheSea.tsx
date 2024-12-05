37b585450ffff6bdd3970db43dd3e11c6fca7375
Bodiu Crinu <144610915+iNeoAzim@users.noreply.github.com>
2024-12-05 20:24:01 +0000

    sauvez la mer

---
+++ src/SaveTheSea.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SaveTheSea.css';
+
+const SaveTheSea: React.FC = () => {
+  return (
+    <div className="save-the-sea">
+      <h1>Sauvez la Mer</h1>
+      <p>L'océan est une partie vitale de notre planète, et c'est à nous de le protéger. Voici quelques façons de vous aider :</p>
+      <ul>
+        <li>Réduire l'utilisation de plastique</li>
+        <li>Soutenir les organisations de conservation marine</li>
+        <li>Participer aux nettoyages de plage</li>
+        <li>Choisir des produits de la mer durables</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SaveTheSea;
