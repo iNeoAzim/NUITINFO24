a3dfc344bc8e44a7427196744f87fe5e1a9d28f9
Your Name <timeo.da-costa@edu.univ-eiffel.fr>
2024-12-05 22:52:24 +0100

    docker

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
      <main>
        <Header />
        <Outlet />
-
+        ggggggggg
      </main>
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import "../assets/style/header.css"
 
 
-export default function Header() {
+export default function Navbar() {
   return (
-    <div>
-       NIQUE TA MER 
-    </div>
-  )
-}
+    <nav style={{ position: "fixed", top: 0, width: "100%", background: "#333", color: "#fff", padding: "10px 20px", zIndex: 1000 }}>
+      <ul style={{ listStyle: "none", display: "flex", gap: "15px", margin: 0, padding: 0 }}>
+        <li><a href="#section1" style={{ color: "#fff", textDecoration: "none" }}>Section 1</a></li>
+        <li><a href="#section2" style={{ color: "#fff", textDecoration: "none" }}>Section 2</a></li>
+        <li><a href="#section3" style={{ color: "#fff", textDecoration: "none" }}>Section 3</a></li>
+      </ul>
+    </nav>
+  );
+};
